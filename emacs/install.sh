46f835ffca2bd3c5fbf76399becc5384fed7db81
blaedj <blaedj@gmail.com>
2017-05-05 16:00:08 -0500

    make the install script more complete

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
+if [ ! -d "~/code/elisp/spacemacs" ]; then
+    git clone https://github.com/syl20bnr/spacemacs ~/code/elisp/spacemacs
+fi
+if [ ! -d "~/.emacs.d/" ]; then
+    ln -s ~/code/elisp/spacemacs ~/.emacs.d
+fi
+ln -s ~/.dotfiles/emacs/.spacemacs ~/.spacemacs
 ln -s ~/.dotfiles/emacs/ ~/.emacs.d/private/
