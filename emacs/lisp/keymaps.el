bd461c8b388edce551d13f64bfe2eca4f904161f
blaedj <blaedj@gmail.com>
2017-05-09 15:57:02 -0500

    some indispensible keymaps

--- emacs/lisp/keymaps.el
+++ emacs/lisp/keymaps.el
@@ -4,6 +4,9 @@
 
 ;;; Code:
 
+(evil-define-key 'hybrid global-map
+  (kbd "\C-w") 'backward-kill-word
+  (kbd "\C-x\C-k") 'kill-region)
 
 (require 'mydefuns ) ;; so we can move these defuns out of the keymap file...
 (global-set-key (kbd "<f5>") 'bcj-revert-buffer-no-confirm)
@@ -12,6 +15,8 @@
 (global-set-key (kbd "\C-x m") 'smex) ; smarter M-x
 (global-set-key "\C-x\C-m" 'smex)     ; smarter M-x
 (global-set-key (kbd "<menu>") 'smex)
+
+(global-set-key (kbd "M-l") 'expand-region)
 
 ;; mac switch meta key
 (defun mac-switch-meta nil
