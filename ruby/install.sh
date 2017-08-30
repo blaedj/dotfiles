b0b004d16598ce7b8b8db5df0a7a94d077dd917a
blaedj <blaedj@gmail.com>
2017-08-30 08:19:42 -0500

    Add rspec and rubocop configs

--- ruby/install.sh
+++ ruby/install.sh
@@ -7,10 +7,10 @@
 #   brew install rbenv > /tmp/rbenv-install.log
 # fi
 
-
-
 # if test ! $(which ruby-build)
 # then
 #   echo "  Installing ruby-build for you."
 #   brew install ruby-build > /tmp/ruby-build-install.log
 # fi
+ln -s ~/.dotfiles/ruby/rspec.symlink ~/.rspec
+ln -s ~/.dotfiles/ruby/rubocop.yml.symlink ~/.rubocop.yml
