a88b0fe6024bb5de87ed891f89a4897ad6a51804
blaedj <blaedj@gmail.com>
2018-02-27 12:14:55 -0600

    go tweaks for a more sane dev env
    automatically install go tools needed for editor integration
    
    Push to git@github.com instead of trying to push to https://github.com
    so that I don't get prompted for username/password
    
    make protobuf one of my default brew packages

--- go/install.sh
+++ go/install.sh
@@ -1,1 +1,9 @@
 export GOPATH=$HOME/code/go
+
+go get -u -v github.com/nsf/gocode
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/golang/protobuf/protoc-gen-go
