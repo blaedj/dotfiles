c88bb21cdd19d1934781857b460702299df2c215
blaedj <blaedj@gmail.com>
2018-09-29 11:37:49 -0500

    update emacs install script to switch to develop branch of spacemacs, even if already cloned

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 if [ ! -d "~/code/elisp/spacemacs" ]; then
     git clone https://github.com/syl20bnr/spacemacs ~/code/elisp/spacemacs
-    git checkout develop # run on the develop branch of spacemacs, as master has a lot of missing bugfixes etc
 fi
+
+# run on the develop branch of spacemacs, as master has a lot of missing bugfixes etc
+(cd ~/code/elisp/spacemacs && git checkout develop )
+
 if [ ! -d "~/.emacs.d/" ]; then
     ln -s ~/code/elisp/spacemacs ~/.emacs.d
 fi
