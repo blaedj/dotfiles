5ca702ab601a296941f106f3dfd3de25382bea51
blaedj <blaedj@gmail.com>
2018-11-02 11:21:02 -0500

    make colors in tmux nice

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,1 +1,5 @@
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+# this sets up the 24bit colors properly, or something.
+# not sure really, but it makes emacs colors in tmux
+# behave properly
+tic -x -o ~/.terminfo ./xterm-24bit.terminfo
