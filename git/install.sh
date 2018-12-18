e09c00697d53ed7e694911f3ea9beab7f4b022f5
blaedj <blaedj@gmail.com>
2018-12-18 14:50:25 -0600

    Add pre-commit hooks to catch debug statements

--- git/install.sh
+++ git/install.sh
@@ -2,3 +2,5 @@
 
 ln -s ~/.dotfiles/gitignore.symlink ~/.gitignore
 ln -s ~/.dotfiles/gitconfig.symlink ~/.gitconfig
+ln -s ~/.dotfiles/git/git-templates/ ~/.git-templates
+# DEBUG: REMOVE ME some gibberish here
