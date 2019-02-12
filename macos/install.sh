486bdf7ce13ede114470fce96741fbc4593e63db
blaedj <blaedj@gmail.com>
2019-02-12 11:05:45 -0600

    update list of brew packages to install

--- macos/install.sh
+++ macos/install.sh
@@ -5,10 +5,11 @@ else
   echo "Homebrew is installed ✅"
 fi
 
-brew install ag curl nodejs diff-so-fancy ispell protobuf bat
+brew install ag curl nodejs diff-so-fancy ispell protobuf bat \
+     tcpflow htop the_silver_searcher tmux tldr tree jq
 
-brew cask install dropbox iterm2 1password dash firefox alfred bartender
-brew cask install spectacle tripmode flux slack emacs bitbar ngrok
+brew cask install dropbox iterm2 1password dash firefox alfred bartender \
+     spectacle tripmode slack emacs bitbar ngrok
 
 cp ./DefaultKeyBinding.dict ~/Library/KeyBindings/
 ./set_defaults.sh
