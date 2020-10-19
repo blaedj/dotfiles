05c110788b67ead2235306ae4540e021c2ba6cea
blaedj <blaedj@gmail.com>
2020-10-19 14:16:33 -0500

    add .gitattributes for better language-specific diff hunk headers
    This commit:
     - adds a .gitattributes file & configuration for better detection of
     function boundaries in 'git diff' hunk headers

--- git/install.sh
+++ git/install.sh
@@ -3,4 +3,5 @@
 ln -s ~/.dotfiles/git/gitignore.symlink ~/.gitignore
 ln -s ~/.dotfiles/git/gitconfig.symlink ~/.gitconfig
 ln -s ~/.dotfiles/git/gitmessage.symlink ~/.gitmessage
+ln -s ~/.dotfiles/git/gitattributes.symlink ~/.gitattributes
 ln -s ~/.dotfiles/git/git-templates/ ~/.git-templates
