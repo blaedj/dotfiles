87fc952e9b324b1d3b2d29d349e304bf686a7b18
blaedj <blaedj@gmail.com>
2021-01-21 13:08:30 -0600

    don't use long flags for ls aliases on darwin

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,13 +1,2 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-# if $(gls &>/dev/null)
-# then
-#   alias ls="gls -F --color"
-#   alias l="gls -lAh --color"
-#   alias ll="gls -l --color"
-#   alias la='gls -A --color'
-# fi
-
 alias git-head="git log --oneline --no-color | head -n 1 | awk '{print $1}'"
 alias fd='fd-find'
--- zsh/lib/directories.zsh
+++ zsh/lib/directories.zsh
@@ -12,8 +12,17 @@ alias -g ......='../../../../..'
 alias 1='cd -'
 alias 2='cd -2'
 
-# List directory contents
-alias ls="ls --color"
-alias l='ls --color -l --all --human-readable'
-alias ll='ls --color -l --human-readable'
-alias la='ls --color --almost-all --human-readable'
+if [[ "$(uname -s)" -eq "Linux" ]]; then
+    alias ls="ls --color"
+    alias l='ls --color -l --all --human-readable'
+    alias ll='ls --color -l --human-readable'
+    alias la='ls --color --almost-all --human-readable'
+fi
+
+# ls on macos seems not to support long flags..?
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    alias ls="ls -c"
+    alias l='ls -clah'
+    alias ll='ls -clh'
+    alias la='ls -cAh'
+fi
