2a24b358dcbc4a4b8568eea36223f4a27ba24f2c
blaedj <blaedj@gmail.com>
2021-11-04 12:26:52 -0500

    install emacs in emacs install.sh

--- emacs/install.sh
+++ emacs/install.sh
@@ -8,6 +8,14 @@ then
  sudo apt install emacs26
 fi
 
+if test ! $(which emacs)
+then
+    echo "installing Emacs from: https://github.com/d12frosted/homebrew-emacs-plus"
+    brew install emacs-plus@28 --with-native-comp --with-xwidgets --with-modern-pen-icon
+    ln -s /opt/homebrew/opt/emacs-plus@28/Emacs.app /Applications
+    # make sure emacsclient is in the path
+    sudo ln -s /opt/homebrew/bin/emacsclient /usr/local/bin/
+fi
 
 if [ ! -d "~/code/elisp/spacemacs" ]; then
     git clone https://github.com/syl20bnr/spacemacs ~/code/elisp/spacemacs
