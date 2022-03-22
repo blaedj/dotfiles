a418a401c5c48c0ac6d880314cf1b3ce1d4b10e7
blaedj <blaedj@gmail.com>
2022-03-22 14:44:25 -0500

    add handy functions for setting slack status
    This commit:
     - adds a new zsh function that can quickly set your slack status.
    
    Why?
     - Once you create an app in the proper workspace, grant it
     permissions, create a user token and store the token in the right
     place, this function makes it easy to set a slack status (e.g.
     'away', 'errand', 'lunch') or clear your slack status.
     - TODO: add a sample app manifest somewhere to make this even easier

---
+++ zsh/slack.zsh
@@ -0,0 +1,64 @@
+
+# set slack status
+function status() {
+    typeset TOKEN_LOCATION=~/.config/slack_status/slack_status_token
+    typeset SLACK_STATUS_API_TOKEN="$(cat $TOKEN_LOCATION 2>/dev/null)"
+    # SLACK_STATUS_SETUP_INSTRUCTIONS=
+    if [[ -z "$SLACK_STATUS_API_TOKEN" ]]; then
+        echo "
+Please store a status token in $TOKEN_LOCATION.
+The token should be from an app installed in your workspace that has
+the following User Token OAuth scopes:
+ - users.profile:read
+ - users.profile:write
+"
+        return;
+    fi
+
+    case $1 in
+        "clear")
+            typeset output=$(curl https://slack.com/api/users.profile.set \
+                                  --silent \
+                                  --request POST \
+                                  --header "Content-Type: application/json; charset=utf-8" \
+                                  --header "Authorization: Bearer $SLACK_STATUS_API_TOKEN" \
+                                  --data '{"profile": {"status_text": "", "status_emoji": ""}}'
+                    )
+            ;;
+        "lunch" )
+            typeset output=$(curl https://slack.com/api/users.profile.set \
+                                  --silent \
+                                  --request POST \
+                                  --header "Content-Type: application/json; charset=utf-8" \
+                                  --header "Authorization: Bearer $SLACK_STATUS_API_TOKEN" \
+                                  --data '{"profile": {"status_text": "lunchtime", "status_emoji": ":chompy:"}}'
+                    )
+            ;;
+
+        "away" )
+            typeset output=$(curl https://slack.com/api/users.profile.set \
+                                  --silent \
+                                  --request POST \
+                                  --header "Content-Type: application/json; charset=utf-8" \
+                                  --header "Authorization: Bearer $SLACK_STATUS_API_TOKEN" \
+                                  --data '{"profile": {"status_text": "afk", "status_emoji": ":away:"}}'
+                    )
+            ;;
+        "errand" )
+            typeset output=$(curl https://slack.com/api/users.profile.set \
+                                  --silent \
+                                  --request POST \
+                                  --header "Content-Type: application/json; charset=utf-8" \
+                                  --header "Authorization: Bearer $SLACK_STATUS_API_TOKEN" \
+                                  --data '{"profile": {"status_text": "errand", "status_emoji": ":car:"}}'
+                    )
+            ;;
+
+        *)
+            echo "please provide a valid command: clear/lunch/errand" ;;
+    esac
+
+    if [[ "$2" == '--debug' ]]; then
+        echo $output
+    fi
+}
