e4f848919afb23d8acec733528d68bcee1c32eb3
blaedj <blaedj@gmail.com>
2022-11-01 09:36:19 -0500

    setup commit signing via ssh key

--- git/install.sh
+++ git/install.sh
@@ -5,3 +5,6 @@ ln -s ~/.dotfiles/git/gitconfig.symlink ~/.gitconfig
 ln -s ~/.dotfiles/git/gitmessage.symlink ~/.gitmessage
 ln -s ~/.dotfiles/git/gitattributes.symlink ~/.gitattributes
 ln -s ~/.dotfiles/git/git-templates/ ~/.git-templates
+
+# for ssh signing of commits
+touch ~/.ssh/allowed_signers
