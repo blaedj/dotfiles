6c445aa31ff55c717ba4956e543e59d5e1ae9d94
blaedj <blaedj@gmail.com>
2023-09-06 10:29:41 -0500

    add helpful comment about nvm errors

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,4 +1,8 @@
 # nvm = node version manager
+
+# All of this assumes that nvm is installed, you'll see errors like:
+# undefined function `nvm_find_nvmrc`
+# or similar if nvm is not installed.
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
