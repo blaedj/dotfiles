963e3991f12900585dd4b270346600d43fcef2e9
blaedj <blaedj@gmail.com>
2024-06-28 10:59:48 -0500

    doom-emacs: increase default font size

--- doom.d/config.el
+++ doom.d/config.el
@@ -28,7 +28,7 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "Anonymous Pro" :size 14))
+(setq doom-font (font-spec :family "Anonymous Pro" :size 15))
 
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
