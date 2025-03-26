22e6f6c2d817a793302239d6a5f24f0b8c44cdc6
blaedj <blaedj@gmail.com>
2025-03-26 09:26:10 -0500

    add notes about unnecessary fzf setup
    this is probably unused and can probably be removed

--- system/install.sh
+++ system/install.sh
@@ -1,4 +1,6 @@
-
+# NOTE: None of this seems to be needed - the file paths described don't exist,
+# and fzf still works fine
+#
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
@@ -13,5 +15,7 @@ ln -s ~/.dotfiles/system/.bashrc ~/.bashrc
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
+# NOTE: this appears to invalid, because this file doesn't exist.
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+# source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$(brew --prefix fzf)/shell/key-bindings.zsh"
