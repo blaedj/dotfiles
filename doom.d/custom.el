664c0edf92dc7d4c08d45a77da7a71898a5c278b
blaedj <blaedj@gmail.com>
2025-04-28 14:24:11 -0500

    doom.d: color strings and enable narrow-to-region

---
+++ doom.d/custom.el
@@ -0,0 +1,7 @@
+;;; custom.el --- custom vars and enabling hidden funcs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  Blaed Johnston
+
+;; Author: Blaed Johnston <blaed@AgilebookPro.local>
+;; Keywords: abbrev, abbrev, abbrev,
+(put 'narrow-to-region 'disabled nil)
