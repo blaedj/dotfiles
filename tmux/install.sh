c2d01f87815b7afcd8787fc7e230c7fc135ae28d
blaedj <blaedj@gmail.com>
2026-03-13 11:46:50 -0500

    add tmux-which-key

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,4 +2,8 @@ ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 # this sets up the 24bit colors properly, or something.
 # not sure really, but it makes emacs colors in tmux
 # behave properly
-tic -x -o ~/.terminfo ./xterm-24bit.terminfo
+tic -x -o ~/.terminfo "$HOME/.dotfiles/tmux/xterm-24bit.terminfo"
+
+if [ ! -d "$HOME/.dotfiles/tmux/tmux-plugins/tmux-which-key" ]; then
+    git clone --recursive https://github.com/alexwforsythe/tmux-which-key "$HOME/.dotfiles/tmux/tmux-plugins/tmux-which-key"
+fi
